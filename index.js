1d09b1d6b56cf23b79c0f1aa36d3bfc36229842e
Hugo Matthews <hugo.matthews@vodafone.com>
2022-10-25 11:40:27 +0100

    3.5 phonebook be step5

--- index.js
+++ index.js
@@ -61,6 +61,25 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
+app.post("/api/persons", (request, response) => {
+  const body = request?.body;
+  if (!body) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+  console.log(Math.floor(Math.random() * 999999));
+  const newPerson = {
+    id: Math.floor(Math.random() * 999999),
+    name: body.name,
+    number: body.number,
+  };
+  console.log(newPerson);
+
+  persons = persons.concat(newPerson);
+  response.json(newPerson);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
