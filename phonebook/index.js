fdb41cf9af88cb947ba2b00f3866eadae51a9f4a
Hugo Matthews <hugo.matthews@vodafone.com>
2022-11-24 19:58:47 +0000

    Notes middleware

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:", request.path);
+  console.log("Body:", request.body);
+  console.log("---");
+  next();
+};
+
 app.use(express.json());
+app.use(requestLogger);
 
 let persons = [
   {
