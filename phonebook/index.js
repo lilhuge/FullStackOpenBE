81d2ccc22d1f5c1f230231363b8f36028277d1f8
Hugo Matthews <hugo.matthews@vodafone.com>
2022-11-24 20:21:40 +0000

    3.7 phonebook be step 7

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,16 +1,17 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
-const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:", request.path);
-  console.log("Body:", request.body);
-  console.log("---");
-  next();
-};
+// const requestLogger = (request, response, next) => {
+//   console.log("Method:", request.method);
+//   console.log("Path:", request.path);
+//   console.log("Body:", request.body);
+//   console.log("---");
+//   next();
+// };
 
 app.use(express.json());
-app.use(requestLogger);
+app.use(morgan("tiny"));
 
 let persons = [
   {
