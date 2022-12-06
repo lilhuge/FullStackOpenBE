bfad09b2eaa9a094f4167a5fd663e0079b931ab8
Hugo Matthews <hugo.matthews@vodafone.com>
2022-12-06 22:21:09 +0000

    Deploying to internet lesson

--- notes/index.js
+++ notes/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const requestLogger = (request, response, next) => {
@@ -9,6 +10,8 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+app.use(express.static("build"));
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
@@ -97,7 +100,7 @@ const unknownEndpoint = (request, response) => {
 };
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
