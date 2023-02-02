8983029cf88a0d14d3a195085f6da28d98173706
Hugo Matthews <hugo.matthews@vodafone.com>
2023-02-02 12:07:01 +0000

    3.13 phonebook database step1

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 const app = express();
 
 morgan.token("requestBody", (req, res) => {
@@ -26,26 +28,26 @@ app.use(express.json());
 app.use(morgan(loggerFunction));
 
 let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
+  // {
+  //   id: 1,
+  //   name: "Arto Hellas",
+  //   number: "040-123456",
+  // },
+  // {
+  //   id: 2,
+  //   name: "Ada Lovelace",
+  //   number: "39-44-5323523",
+  // },
+  // {
+  //   id: 3,
+  //   name: "Dan Abramov",
+  //   number: "12-43-234345",
+  // },
+  // {
+  //   id: 4,
+  //   name: "Mary Poppendieck",
+  //   number: "39-23-6423122",
+  // },
 ];
 
 app.get("/info", (request, response) => {
@@ -60,20 +62,28 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((people) => {
+    response.json(people);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
+  // const id = Number(request.params.id);
 
-  const person = persons.find((person) => person.id === id);
-  console.log(person);
+  // const person = persons.find((person) => person.id === id);
+  // console.log(person);
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  // if (person) {
+  //   response.json(person);
+  // } else {
+  //   response.status(404).end();
+  // }
+  Person.findById(request.params.id)
+    .then((person) => response.json(person))
+    .catch((error) => {
+      console.log(error);
+      response.status(404).end();
+    });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
