4b6fe942d0cba9220f8aea0b3d81c07e4f1633cb
Hugo Matthews <hugo.matthews@vodafone.com>
2023-02-15 18:05:48 +0000

    3.19 phonebook database step 7

--- notes/index.js
+++ notes/index.js
@@ -46,8 +46,7 @@ app.get("/api/notes/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
-  //console.log(request.headers);
+app.post("/api/notes", (request, response, next) => {
   const body = request?.body;
   if (!body.content) {
     return response.status(400).json({
@@ -60,9 +59,12 @@ app.post("/api/notes", (request, response) => {
     important: body.important || false,
     date: new Date(),
   });
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/notes/:id", (request, response, next) => {
@@ -73,15 +75,14 @@ app.delete("/api/notes/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.put("api/notes/:id", (request, response, next) => {
-  const body = request.body;
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    imporant: body.important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedNote) => {
       response.json(updatedNote);
     })
@@ -99,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
--- phonebook/index.js
+++ phonebook/index.js
@@ -62,7 +62,7 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request?.body;
   if (!body) {
     return response.status(400).json({
@@ -96,20 +96,24 @@ app.post("/api/persons", (request, response) => {
     number: newNumber,
   });
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
-  const body = request.body;
+  const { name, number } = request.body;
 
-  person = {
-    name: body.name,
-    number: body.number,
-  };
+  person = { name, number };
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators,
+    context: "query",
+  })
     .then((updatedPerson) => {
       response.json(updatedPerson);
     })
@@ -121,6 +125,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if ((error.name = "ValidationError")) {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };
