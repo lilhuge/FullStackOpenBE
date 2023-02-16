4c575a2cc738fba10b8e8975de4b5a8da1eb6a16
Hugo Matthews <hugo.matthews@vodafone.com>
2023-02-16 10:43:11 +0000

    3.20 phonebook database step8

--- phonebook/models/person.js
+++ phonebook/models/person.js
@@ -18,7 +18,17 @@ mongoose
 
 const personSchema = new mongoose.Schema({
   name: { type: String, minLength: 3, required: true },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (v) => {
+        return /^\d{2}-\d{6,}|^\d{3}-\d{5,}/.test(v);
+      },
+      message: "Not a valid phone number",
+    },
+    required: [true, "User phone number required"],
+  },
 });
 
 personSchema.set("toJSON", {
